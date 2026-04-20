ad3fa22ffad33f3e6932e1d11c948e1e76807d93
admin <hello@bloomvalley.shop>
2026-04-20 17:22:39 +0200

    (feat): added categorydto

---
+++ ZaroShop.Server/Models/DTOs/CategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using ZaroShop.Server.Models.Entities;
+
+namespace ZaroShop.Server.Models.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int? ParentCategoryId { get; set; }
+        public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+
+    }
+}
